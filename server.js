544228bcdc5738e3a3539557a0725f81ef7b5a23
gvozdyk <gvozdyk23@gmail.com>
2022-02-13 20:29:25 +0200

    included socket io

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
+
 const app = express();
+const server = require('http').Server(app);
+const io = require("socket.io")(server);
 
-const rooms = {
-    'rooms': [],
-    'messages': []
-}
+const rooms = new Map();
 
-app.get('/users', function(req, res) {
-    res.send('response');
+app.get('/rooms', (req, res) => {
+    res.json(rooms);
 });
 
-app.listen(6666)
+io.on('connection', socket => {
+    console.log('user connected', socket.id);
+})
+
+server.listen(9999, (err) => {
+    if (err) {
+        throw Error(err);
+    }
+    console.log("Server is Started");
+})
--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import io from 'socket.io-client';
 
-function App() {
+const socket = io("http://localhost:9999/");
+
+const App = () => {
   return (
     <div className="App">
       <h1>Hello</h1>
