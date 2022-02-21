b96fe34fb7ec51074ee7f75c577dd65f3943d42d
gvozdyk <gvozdyk23@gmail.com>
2022-02-22 00:51:35 +0200

    add messages

--- src/App.js
+++ src/App.js
@@ -34,7 +34,10 @@ const App = () => {
     });
     socket.emit("ROOM:JOIN", obj);
     const { data } = await axios.get(`/rooms/${obj.roomId}`);
-    setUsers(data.users);
+    dispatch({
+      type: "SET_DATA",
+      payload: data
+    })
   };
 
   useEffect(() => {
